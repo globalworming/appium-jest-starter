816235201d05f5c20e779ded5f1161a8829bbb39
globalworming <andreas.worm@headissue.com>
2023-03-15 15:12:27 +0100

    first test running

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-## run this
+# appium jest
 
 ## from scratch
 * Linux
 * Node v19.7.0
-
-
+ 
+install the dependencies
 ```shell
-npm install --save-dev jest appium@next webdriverio appium-uiautomator2-driver
+npm install --save-dev jest jest-junit appium@next webdriverio appium-uiautomator2-driver
 ```
-```shell
-npm install --save-dev jest appium@next webdriverio appium-uiautomator2-driver
-```
+
+install this driver for appium
 ```shell
 npm run env -- appium driver install uiautomator2
 ```
 
+get the android sdk tools
 ```shell
 wget https://dl.google.com/android/repository/platform-tools_r34.0.0-linux.zip -O /tmp/android_sdk.zip
 unzip /tmp/android_sdk.zip -d sdk
@@ -25,25 +25,40 @@ unzip /tmp/commandlinetools.zip -d .
 mkdir -p sdk/cmdline-tools
 mv cmdline-tools sdk/cmdline-tools/tools
 rm /tmp/commandlinetools.zip
-mkdir sdk platforms
+mkdir sdk/platforms
 ```
+
+accept licenses, install dependencies and a system image
 ```shell
 yes | sdk/cmdline-tools/tools/bin/sdkmanager --licenses --sdk_root=sdk
 sdk/cmdline-tools/tools/bin/sdkmanager --install "build-tools;33.0.2"  --sdk_root=sdk
 echo Y | sdk/cmdline-tools/tools/bin/sdkmanager --install "system-images;android-33;google_apis;x86_64"  --sdk_root=sdk
 sdk/cmdline-tools/tools/bin/sdkmanager --list_installed --sdk_root=sdk
 ```
+
+create a virtual device
 ```shell
-sdk/cmdline-tools/tools/bin/avdmanager --verbose create avd --force --name "pixel4" --package "system-images;android-33;google_apis;x86_64" --device "pixel_4" 
+sdk/cmdline-tools/tools/bin/avdmanager --verbose create avd --force --name "car" --package "system-images;android-33;google_apis;x86_64" --device "automotive_1024p_landscape" 
 ```
 
+start the emulator
 ```shell
-ANDROID_SDK_ROOT=./sdk/ sdk/emulator/emulator -avd pixel4
+ANDROID_SDK_ROOT=./sdk/ sdk/emulator/emulator -avd car
 ```
 
+install the app to use it in your emulator
 ```shell
-npm run appium
+sdk/platform-tools/adb install apk/lambdatest_proverbial_android.apk
 ```
+
+start the appium server
+```shell
+npm run appium 
+```
+
 ```shell
 npm run test
 ```
+
+## locating elements
+get the appium inspector or use the browser version https://inspector.appiumpro.com/, get the capabilities json from src/first.test.js to connect.
--- src/first.test.js
+++ src/first.test.js
@@ -1,11 +1,11 @@
 const {remote} = require('webdriverio');
 
 const capabilities = {
-    platformName: 'Android',
-    'appium:automationName': 'UiAutomator2',
-    'appium:deviceName': 'Android',
-    'appium:appPackage': 'com.android.settings',
-    'appium:appActivity': '.Settings',
+    "platformName": "Android",
+    "appium:automationName": "UiAutomator2",
+    "appium:deviceName": "Android",
+    "appium:appPackage": "com.lambdatest.proverbial",
+    "appium:appActivity": ".MainActivity",
 };
 
 const wdOpts = {
@@ -15,17 +15,65 @@ const wdOpts = {
     capabilities,
 };
 
-describe('test', () => {
+let driver = undefined
+
+beforeAll(async () => {
+    driver = await remote(wdOpts);
+});
+
+afterAll(async () => {
+    await driver.deleteSession();
+});
+
+// https://github.com/LambdaTest/LT-appium-nodejs-webdriverio/blob/master/specs/android-test.js
+describe('Proverbial APK', () => {
     describe("in detail", () => {
-        it ("should do something", async () => {
-            const driver = await remote(wdOpts);
-            try {
-                const batteryItem = await driver.$('//*[@text="Battery"]');
-                await batteryItem.click();
-            } finally {
-                await driver.pause(1000);
-                await driver.deleteSession();
-            }
+        it("should do something", async () => {
+            expect(driver).not.toBe(undefined)
+            const welcomeMessage = await driver.$("id:Textbox");
+            expect(await welcomeMessage.getText()).toBe("Hello! Welcome to lambdatest Sample App called Proverbial")
         })
+
+        it("Changes color", async () => {
+            const color = await driver.$("id=color");
+            await color.click();
+            await color.click();
+        });
+
+        it("Changes text", async () => {
+            const text = await driver.$("id=Text");
+            await text.click();
+        });
+
+        it("Toast", async () => {
+            const toast = await driver.$("id=toast");
+            await toast.click();
+        });
+
+        it("Notification", async () => {
+            const nf = await driver.$("id=com.lambdatest.proverbial:id/notification");
+
+            await nf.click();
+            await driver.$("id=com.android.permissioncontroller:id/permission_allow_button").then(it => it.click());
+            await nf.click();
+            await driver.openNotifications();
+            await driver.$('//android.widget.TextView[@text="Test Notification"]');
+            await driver.back();
+
+        });
+
+        it("Geolocation", async () => {
+            const geo = await driver.$("id=geoLocation");
+            await geo.click();
+
+            await driver.back();
+        });
+
+        it("SpeedTest", async () => {
+            const st = await driver.$("id=speedTest");
+            await st.click();
+
+            await driver.back();
+        });
     });
 });
